f4b72696ac5610c630d5924c7c3e325ab635e64c
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2024-01-11 12:07:15 +0300

    Added task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module for do_pack function"""
+from datetime import datetime
+from fabric.api import local
+import os
+
+
+def do_pack():
+    """
+    Script that generates a .tgz archive
+    from the contents of the web_static folder
+    """
+    now = datetime.now().strftime("%Y%m%d%H%M%S")
+    if not os.path.isdir("versions"):
+        local("mkdir versions")
+    local("tar -cvzf ./versions/web_static_{}.tgz ./web_static/".format(now))
+    return "./versions/web_static_{}.tgz".format(now)
