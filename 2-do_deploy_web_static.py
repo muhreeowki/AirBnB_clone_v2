620c590d482b13fd063a605c551aa7dbb19e3db2
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2024-01-11 15:11:10 +0300

    Added task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -15,42 +15,42 @@ def do_pack():
     from the contents of the web_static folder
     """
     now = datetime.now().strftime("%Y%m%d%H%M%S")
-    try:
-        if not os.path.isdir("versions"):
-            local("mkdir versions")
-        local("tar -cvzf versions/web_static_{}.tgz web_static".format(now))
-        return "versions/web_static_{}.tgz".format(now)
-    except Exception:
-        return None
+    if not os.path.isdir("versions"):
+        local("mkdir versions")
+    result = local("tar -cvzf versions/web_static_{}.tgz web_static".format(now))
+    return "versions/web_static_{}.tgz".format(now) if result else None
 
 
 def do_deploy(archive_path):
     """
     Script that distributes an archive to my web servers
     """
-    try:
-        if not os.path.exists(archive_path):
-            return False
-        name = archive_path.split("/")[1]
-        name = name.split(".")[0]
-        put(local_path=archive_path, remote_path="/tmp/")
-        sudo(f"mkdir -p /data/web_static/releases/{name}/")
-        sudo("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}".format(name, name))
-        sudo(f"rm /tmp/{name}.tgz")
-        sudo(
-            "cp -R /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/".format(
-                name, name
-            )
-        )
-        sudo(f"rm -rf /data/web_static/releases/{name}/web_static")
-        sudo("rm /data/web_static/current")
-        sudo(
-            "ln -s /data/web_static/releases/{} \
-            /data/web_static/current".format(
-                name
-            )
-        )
-        return True
-    except Exception:
+    if not os.path.exists(archive_path):
         return False
+    name = archive_path.split("/")[1]
+    name = name.split(".")[0]
+    put(local_path=archive_path, remote_path="/tmp/")
+    sudo(f"mkdir -p /data/web_static/releases/{name}/")
+    sudo(
+        "tar -xzf /tmp/{}.tgz -C \
+        /data/web_static/releases/{}".format(
+            name, name
+        )
+    )
+    sudo(f"rm /tmp/{name}.tgz")
+    sudo(
+        "cp -R /data/web_static/releases/{}/web_static/* \
+        /data/web_static/releases/{}/".format(
+            name, name
+        )
+    )
+    sudo(f"rm -rf /data/web_static/releases/{name}/web_static")
+    sudo("rm /data/web_static/current")
+    sudo(
+        "ln -s /data/web_static/releases/{} \
+        /data/web_static/current".format(
+            name
+        )
+    )
+    print("New version deployed!")
+    return True
